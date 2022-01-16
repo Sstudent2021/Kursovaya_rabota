7a4676ddc6d76239e32687bc2b5b9a16496530f8
Sstudent2021 <zsiergiej@yandex.ru>
2022-01-16 15:01:05 -0700

    Добавлена функция поиска email (по знаку @)

---
+++ Kurs_rabota_01.py
@@ -0,0 +1,87 @@
+# Программа анализа .csv файла - 2
+
+import tkinter as tk
+from tkinter.scrolledtext import ScrolledText as st
+from tkinter import messagebox as mb
+from tkinter import filedialog as fd
+import os
+import pandas as pd
+
+# Создание главного окна
+window = tk.Tk()
+window.geometry("550x550")
+window.title("Программа анализа .csv файла - 2")
+
+# Создание меток вывода
+label_00 = tk.Label(text = "Файл:")
+label_00.grid(row=0, column=0, padx=10, pady=10, sticky="e")
+
+label_01 = tk.Label(text = "")
+label_01.grid(row=0, column=1, sticky="w")
+
+label_10 = tk.Label(text = "Строк:")
+label_10.grid(row=1, column=0, padx=10, pady=10, sticky="e")
+
+label_11 = tk.Label(text = "")
+label_11.grid(row=1, column=1, sticky="w")
+
+label_20 = tk.Label(text = "Столбцов:")
+label_20.grid(row=2, column=0, padx=10, pady=10, sticky="e")
+
+label_21 = tk.Label(text = "")
+label_21.grid(row=2, column=1, sticky="w")
+
+# Создание текстового вывода с прокруткой
+output_text = st(height = 22, width = 50)
+output_text.grid(row=3, column=1, padx=10, pady=10, sticky="w")
+
+# Диалог открытия файла
+def do_dialog():
+    my_dir = os.getcwd()
+    name= fd.askopenfilename(initialdir=my_dir)
+    return name
+
+# csv pandas
+def pandas_read_csv(file_name):
+    df = pd.read_csv(file_name, header=None, sep=';')
+    cnt_rows = df.shape[0]
+    cnt_colums = df.shape[1]
+    label_11['text'] = cnt_rows
+    label_21['text'] = cnt_colums
+    return df
+
+# Выборка столбца в список
+def get_column(df, column_ix):
+    cnt_rows = df.shape[0]
+    lst = []
+    for i in range(cnt_rows):
+        lst.append(df.iat[i,column_ix])
+    return lst
+    
+# Если в этом поле email (по знаку @), пусть вернет True
+def meet_email(field):
+    checkfor = ['@']
+    for s in checkfor:
+        if s in str(field): # Нашлось!
+            return True
+    # Ничего не совпало
+    return False
+
+# Оообработчик нажатия кнопки
+def process_button():
+    file_name = do_dialog()
+    label_01['text'] = file_name
+    df = pandas_read_csv(file_name)
+    lst = get_column(df, 2)
+    for item in lst:
+        output_text.insert(tk.END, str(item) + '   ' 
+            + str(meet_email(item)) + os.linesep)
+        
+    mb.showinfo(title=None, message="Готово!")
+
+# Создание кнопки
+button = tk.Button(window, text = "Прочитать файл", command=process_button)
+button.grid(row=4, column=1)
+
+# Запуск цикла mainloop
+window.mainloop()
